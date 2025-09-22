36b0a81b2ea9edf6e4ddd6a93c7d4ad156487123
Samson Madu <samsonmadu17@gmail.com>
2025-09-22 12:08:18 +0000

    Checkpoint from VS Code for coding agent session

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import AvatarCustomizeNew from "./pages/AvatarCustomizeNew";
+import MainMenu from "./pages/MainMenu";
+import NotFound from "./pages/not-found";
 
 function Home() {
   return (
@@ -8,6 +10,7 @@ function Home() {
       <h1>HoopDreams</h1>
       <p>New pixel avatars are wired. Go customize:</p>
       <p><Link to="/customize-new">Customize Appearance (Pixel)</Link></p>
+      <p><Link to="/play">Play</Link></p>
     </div>
   );
 }
@@ -18,6 +21,8 @@ export default function App(){
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/customize-new" element={<AvatarCustomizeNew/>} />
+        <Route path="/play" element={<MainMenu/>} />
+        <Route path="*" element={<NotFound/>} />
       </Routes>
     </BrowserRouter>
   );
